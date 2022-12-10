27bec864d2aa45dde33d07464f2746c28fabaf68
PhiHung1503 <phihung15032001@gmail.com>
2022-12-10 15:15:58 +0700

    add sendPasswordReset and logout  function to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -78,6 +78,19 @@ const registerWithEmailAndPassword = async (name, email, password) => {
     }
 };
 
+const sendPasswordReset = async (email) => {
+    try {
+        await sendPasswordResetEmail(auth, email);
+        alert("Password reset link sent!");
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
+const logout = () => {
+    signOut(auth);
+};
 
 export {
     auth,
@@ -85,4 +98,6 @@ export {
     signInWithGoogle,
     logInWithEmailAndPassword,
     registerWithEmailAndPassword,
+    sendPasswordReset,
+    logout,
 };
