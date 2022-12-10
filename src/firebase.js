89ee321847ca360fd05ab22af77cb51094944d63
PhiHung1503 <phihung15032001@gmail.com>
2022-12-10 15:12:39 +0700

    add registerWithEmailAndPassword function to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -62,9 +62,27 @@ const logInWithEmailAndPassword = async (email, password) => {
     }
 };
 
+const registerWithEmailAndPassword = async (name, email, password) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await addDoc(collection(db, "users"), {
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        });
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
+
 export {
     auth,
     db,
     signInWithGoogle,
     logInWithEmailAndPassword,
+    registerWithEmailAndPassword,
 };
