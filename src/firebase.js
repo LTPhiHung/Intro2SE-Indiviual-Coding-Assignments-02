48c2867cf73149cff08aab03286c41156bc2ae5b
PhiHung1503 <phihung15032001@gmail.com>
2022-12-10 15:09:36 +0700

    add logInWithEmailAndPassword  function to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -53,9 +53,18 @@ const signInWithGoogle = async () => {
     }
 };
 
+const logInWithEmailAndPassword = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
 export {
     auth,
     db,
     signInWithGoogle,
-
+    logInWithEmailAndPassword,
 };
